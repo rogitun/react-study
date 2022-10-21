3bf159649d2fed093c46001e0387d5178f374056
hansel <rogitun@gmail.com>
2022-10-22 01:05:58 +0900

    use-state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./App.css"
+
+
+const labeStyle = {
+  color : "red"
+}
 
 function App() {
+
+  const textChange = (e) =>{
+    setText(e.target.value);
+}
+
+  const [inputText,setText] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>My name is Hansel</h1>
+
+        <ul>
+          <li className="label-highlight">age : </li>
+          <li className="label-highlight"> city : </li>
+
+          <label className="label-highlight">what you want</label><br></br>
+          <textarea onChange={textChange}></textarea>
+        </ul>
+
+        <label style={labeStyle}>what you type</label>
+        <p>{inputText}</p>
+
+
+        <p>현재 글자 수 : {inputText.length}</p>
     </div>
   );
 }
 
 export default App;
+
